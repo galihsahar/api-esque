7137d578522328a735d124259bb6a26b5d1f2213
Galih Sahar Oktavianto <galih.oktavianto@ogya.co.id>
2023-06-26 13:42:00 +0700

    update cors

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-const db = require('./queries') 
+const db = require('./queries')
 const cors = require('cors')
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://www.esque.co.id'],
+}));
 
 app.use(bodyParser.json())
 app.use(
-  bodyParser.urlencoded({ 
+  bodyParser.urlencoded({
     extended: true,
   })
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
 app.get('/user/findByUserName', db.getUserByUsername)
@@ -45,5 +47,5 @@ app.put('/team/update', db.editDataTeam);
 app.delete('/team/delete', db.deleteDataTeam);
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+  console.log(`App running on port ${port}.`)
 })
